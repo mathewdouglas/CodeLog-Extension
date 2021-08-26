1b38eff05bd437058c175fb3dbaa942730b8d479
Mathew Douglas <mathewdouglas92@gmail.com>
2021-08-26 15:14:07 +1200

    Version 0.1

--- codelog/extension.js
+++ codelog/extension.js
@@ -1,28 +1,48 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const axios = require('axios');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
+	var result = await axios.get("https://mathewdouglas.co.za/CodeLog/data.json");
+	var json = result.data.snippets;
+	var languages = [json.length - 1];
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "codelog" is now active!');
+	for (let i = 1; i < json.length; i++) {
+		const element = json[i];
+		languages[i-1] = {
+			"label": element[0].language,
+			"data": element.map(item => {
+				return {
+					label: item.title,
+					detail: item.content,
+				}
+			}),
+		};
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('codelog.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CodeLog!');
-	});
+	// console.log(result.data);
+
+	vscode.window.showInformationMessage('CodeLog Snippets is up and running');
+
+	let disposable = vscode.commands.registerCommand(
+		'codelog.getSnippet',
+		async function () {
+			const snippet = await vscode.window.showQuickPick(languages, {
+				matchOnDetail: true
+			})
+			console.log(snippet);
+
+			if (snippet == null) return
+
+			const item = await vscode.window.showQuickPick(snippet.data, {
+				matchOnDetail: true
+			})
+			console.log(item);
+		}
+	);
 
 	context.subscriptions.push(disposable);
 }
